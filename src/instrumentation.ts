56f03bdc6b9144ab376abff99bc10c9eb94ea46b
JMonde <joaquinmondejar24@gmail.com>
2025-11-13 23:19:53 +0100

    2

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,3 @@
-'use client';
-
 // This file is used to configure instrumentation for the application
 // In our case, we're not adding any custom instrumentation,
 // but having this file helps Next.js know that the app should be handled appropriately
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,6 @@
+// TODO: Next.js 15.5+ deprecation warning - middleware will be replaced by proxy in Next.js 16
+// For now, keeping as middleware to maintain auth functionality
+// Future migration should move auth logic to data layer and use proxy for routing only
 import { NextResponse, type NextRequest } from "next/server";
 import { chain } from "./middlewares/compose";
 import { withAuth } from "./middlewares/withAuth";
@@ -11,7 +14,7 @@ export default async function middleware(req: NextRequest) {
       cookieName: REFRESH_COOKIE_NAME,
     }),
   );
-  
+
   return await handler(req);
 }
 
